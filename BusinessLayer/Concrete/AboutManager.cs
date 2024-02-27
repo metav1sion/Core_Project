9e83dd573be44ba09a4d67c5a59cc33022cd25e9
Metehan Sözen <metehansozen@outlook.com>
2024-02-27 17:00:44 +0300

    ViewCompanent eklendi

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+	public class AboutManager : IAboutService
+	{
+		
+		private IAboutDal _aboutDal;
+
+		private AboutManager(IAboutDal aboutDal)
+		{
+			_aboutDal = aboutDal;
+		}
+
+		public void TAdd(About t)
+		{
+			_aboutDal.Insert(t);
+		}
+
+		public void TDelete(About t)
+		{
+			_aboutDal.Delete(t);
+		}
+
+		public void TUpdate(About t)
+		{
+			_aboutDal.Update(t);
+		}
+
+		public List<About> TGetList()
+		{
+			return _aboutDal.GetList();
+
+		}
+
+		public About TGetByID(int id)
+		{
+			return _aboutDal.GetByID(id);
+		}
+	}
+}
