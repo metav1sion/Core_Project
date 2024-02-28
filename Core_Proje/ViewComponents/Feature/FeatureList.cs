853658b8c42ed8565648f74131c2ccaa9b115fb3
Metehan Sözen <metehansozen@outlook.com>
2024-02-28 15:52:17 +0300

    Feature kısmı dinamik hale getirildi.

--- Core_Proje/ViewComponents/Feature/FeatureList.cs
+++ Core_Proje/ViewComponents/Feature/FeatureList.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.ViewComponents.Feature
 {
     public class FeatureList : ViewComponent
     {
+        private FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = featureManager.TGetList();
+            return View(values);
         }
     }
 }
