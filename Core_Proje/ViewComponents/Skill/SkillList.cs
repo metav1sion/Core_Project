8a0997901e27ad4a094b936858880fe376635cb3
Metehan Sözen <metehansozen@outlook.com>
2024-03-05 11:13:20 +0300

    Skill kısmı yapıldı, sayfadaki küçük hatalar düzeltildi

---
+++ Core_Proje/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Skill
+{
+    public class SkillList : ViewComponent
+    {
+        private SkillManager skill = new SkillManager(new EfSkillDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = skill.TGetList();
+            return View(values);
+        }
+    }
+}
