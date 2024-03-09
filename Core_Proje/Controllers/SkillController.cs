f7a299ba37adc2b84631b9c0f25d4ab41c9dfa9b
Metehan Sözen <metehansozen@outlook.com>
2024-03-10 02:32:31 +0300

    Admin Controller eklendi, gerekli partiallar oluşturuldu.

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class SkillController : Controller
     {
+        private SkillManager manager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
-            return View();
+            var values = manager.TGetList();
+            return View(values);
         }
+        
     }
 }
