2f0a7b806478443fb073b1eedbca5dd98c356b71
Metehan Sözen <metehansozen@outlook.com>
2024-03-20 16:45:57 +0300

    dashboard için istatistik kodları yazıldı

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,18 @@
+﻿using DataAccesLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        private Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x=>x.Status == false).Count();
+            ViewBag.v3 = c.Messages.Where(x=>x.Status == true).Count();
+            ViewBag.v = c.Experiences.Count();
+            return View();
+        }
+    }
+}
